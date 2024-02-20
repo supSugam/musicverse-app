89b27513d64e9e1e403e74dffa974bb286b86c67
supSugam <sugamsubedi234@gmail.com>
2024-02-20 16:51:38 +0545

    Refactor tab bar icons and add animation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,65 @@
-import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Platform } from 'react-native';
+import { Platform, Animated } from 'react-native';
 import Home from './home';
-import { LinearGradient } from 'expo-linear-gradient';
 import COLORS from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import StyledText from '@/components/reusables/StyledText';
 import UploadStackScreen from '../screens/upload';
+import { useEffect } from 'react';
 
 const Tab = createBottomTabNavigator();
+
+const TabBarIcon = ({ focused, route }: { focused: boolean; route: any }) => {
+  let iconName: keyof typeof Ionicons.glyphMap = 'home';
+
+  const size = 24;
+  const color = focused ? COLORS.neutral.light : COLORS.neutral.normal;
+  const scale = new Animated.Value(1);
+  const translateY = new Animated.Value(0);
+
+  if (route.name === 'Home') {
+    iconName = focused ? 'home' : 'home-outline';
+  } else if (route.name === 'Feed') {
+    iconName = focused ? 'grid' : 'grid-outline';
+  } else if (route.name === 'Search') {
+    iconName = focused ? 'search' : 'search-outline';
+  } else if (route.name === 'MyLibrary') {
+    iconName = focused ? 'library' : 'library-outline';
+  } else if (route.name === 'Upload') {
+    iconName = focused ? 'add-circle' : 'add-circle-outline';
+  }
+
+  Animated.parallel([
+    Animated.spring(scale, {
+      toValue: focused ? 1.2 : 1,
+      useNativeDriver: true,
+    }),
+    Animated.spring(translateY, {
+      toValue: focused ? -10 : 0,
+      useNativeDriver: true,
+    }),
+  ]).start();
+
+  return (
+    <Animated.View
+      style={{
+        transform: [{ scale }, { translateY }],
+      }}
+    >
+      <Ionicons name={iconName} size={size} color={color} />
+    </Animated.View>
+  );
+};
 
 export default function TabsLayout() {
   return (
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName: keyof typeof Ionicons.glyphMap = 'home';
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon focused={focused} route={route} />
+        ),
 
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Feed') {
-            iconName = focused ? 'grid' : 'grid-outline';
-          } else if (route.name === 'Search') {
-            iconName = focused ? 'search' : 'search-outline';
-          } else if (route.name === 'MyLibrary') {
-            iconName = focused ? 'library' : 'library-outline';
-          } else if (route.name === 'Upload') {
-            iconName = focused ? 'add-circle' : 'add-circle-outline';
-          }
-
-          return (
-            <Ionicons
-              name={iconName}
-              size={size}
-              color={focused ? COLORS.neutral.white : COLORS.neutral.normal}
-            />
-          );
-        },
         tabBarLabel: ({ focused, color }) => {
           const routeName = route.name as string;
           return (
@@ -63,7 +85,6 @@ export default function TabsLayout() {
           paddingTop: 10,
           height: 70,
           backgroundColor: 'transparent',
-          // position: 'absolute', // this makes tab bar take position
           borderTopWidth: 0,
           elevation: 0,
         },
@@ -73,33 +94,6 @@ export default function TabsLayout() {
           animationEnabled: true,
         },
         tabBarHideOnKeyboard: true,
-        tabBarBackground: () => (
-          <LinearGradient
-            colors={[
-              'rgba(0, 0, 0, 0.05)',
-              'rgba(0, 0, 0, 0.1)',
-              'rgba(0, 0, 0, 0.2)',
-              'rgba(0, 0, 0, 0.25)',
-              'rgba(0, 0, 0, 0.3)',
-              'rgba(0, 0, 0, 0.35)',
-              'rgba(0, 0, 0, 0.5)',
-              'rgba(0, 0, 0, 0.7)',
-              'rgba(0, 0, 0, 0.8)',
-              'rgba(0, 0, 0, 0.9)',
-            ]}
-            start={[0, 0]}
-            end={[0, 1]}
-            style={{
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              top: 0,
-              height: '100%',
-              flex: 1,
-              zIndex: -1,
-            }}
-          />
-        ),
       })}
     >
       <Tab.Screen
@@ -113,22 +107,21 @@ export default function TabsLayout() {
         name="Search"
         component={Home}
         options={{
-          headerTitle: 'Home',
+          headerTitle: 'Search',
         }}
       />
       <Tab.Screen
         name="Feed"
         component={Home}
         options={{
-          headerTitle: 'Home',
+          headerTitle: 'Feed',
         }}
       />
-
       <Tab.Screen
         name="MyLibrary"
         component={Home}
         options={{
-          headerTitle: 'Home',
+          headerTitle: 'My Library',
         }}
       />
       <Tab.Screen
